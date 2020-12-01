2c80bfd5a234c0db533607968e29cade1bf27bea
gitqoo <41725249+gitqoo@users.noreply.github.com>
2020-12-01 16:26:48 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
+yum install docker -y
 ssh-keygen -t rsa -b 4096 -f ssh_host_rsa_key -P "" < /dev/null
 ssh-keygen -t ed25519 -f ssh_host_ed25519_key -P ""  < /dev/null
-
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&  sudo chmod +x /usr/local/bin/docker-compose  && docker-compose --version && systemctl restart docker && systemctl enable  docker
 
 mkdir -p /efs/sftp/home
